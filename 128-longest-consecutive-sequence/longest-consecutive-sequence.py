0d652e23ff8bd65c76ff49427a60cf5fb6596394
thisshekhar <34572158+thisshekhar@users.noreply.github.com>
2026-08-07 07:01:00 +0530

    Time: 55 ms (52.01%) | Memory: 26.9 MB (71.46%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -4,26 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # Convert the list to a set for O(1) lookups. 
-        # This is the key to achieving O(n) overall time complexity.
         num_set = set(nums)
-        longest_streak = 0
-
-        # Iterating over the set is slightly faster as it removes duplicates
+        longest = 0
         for num in num_set:
-            # Only start checking if it's the absolute beginning of a sequence.
-            # If num - 1 is in the set, we already counted this sequence!
-            if (num - 1) not in num_set:
-                current_num = num
-                current_streak = 1
-
-                # Count upwards to find the end of the current consecutive sequence
-                while (current_num + 1) in num_set:
-                    current_num += 1
-                    current_streak += 1
-
-                # Update our max length found so far
-                longest_streak = max(longest_streak, current_streak)
-
-        return longest_streak
+            if (num -1) not in num_set:
+                x = num
+                cnt = 1
+                while (x+1)  in num_set:
+                    cnt = cnt + 1
+                    x = x + 1
+                longest = max(cnt,longest)
+        return longest
 
