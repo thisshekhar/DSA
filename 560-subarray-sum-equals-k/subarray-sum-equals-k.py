d9964d1b68aa621025e856c55212aa34d04fb212
thisshekhar <34572158+thisshekhar@users.noreply.github.com>
2026-08-08 16:59:06 +0530

    Time: 40 ms (43.31%) | Memory: 14.7 MB (46.41%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -5,15 +5,13 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        count  = 0
+        count =- 0
+        prefixSum_frq = {0:1}
         current_sum = 0
-        prefix_frq = {0:1}
         for num in nums:
             current_sum += num
-            target = current_sum  - k
-            if(target in prefix_frq):
-                count += prefix_frq[target]
-            prefix_frq[current_sum] = prefix_frq.get(current_sum,0) + 1
+            target = current_sum - k
+            if(target in prefixSum_frq):
+                count += prefixSum_frq[target]
+            prefixSum_frq[current_sum] = prefixSum_frq.get(current_sum,0) + 1
         return count
-
-            
